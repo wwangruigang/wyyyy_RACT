0ebfcae573dd8f2f7065191fd481b44992ea8316
wangruigang <wangruigang_web@163.com>
2020-07-27 17:17:03 +0800

    使用react框架和网易云接口练习模仿网易云音乐（PC）

---
+++ src/views/Songs/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Songs extends Component {
+  render () {
+    return (
+      <div>
+        暂未开发
+      </div>
+    );
+  }
+}
+
+export default Songs;
